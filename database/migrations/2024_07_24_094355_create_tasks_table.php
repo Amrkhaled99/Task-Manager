8761f99b34526bf1373a4fcadeae8f0b2e4ae96b
amrkhaled99 <amrkh5255@gmail.com>
2024-07-24 13:30:14 +0300

    Generate project basic structure including migrations, models, and controllers

--- database/migrations/2024_07_24_094355_create_tasks_table.php
+++ database/migrations/2024_07_24_094355_create_tasks_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('assigned_to_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('assigned_by_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
